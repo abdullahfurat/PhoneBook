a932754463ef8b6f31f44989ccad4fc636d071ed
muratvuranok <mrtvrnk@gmail.com>
2020-01-05 13:56:36 +0300

    Add project files.

---
+++ PhoneAppp/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace PhoneAppp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.txtSoyadi = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lstKisiler = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmDuzenle = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmSil = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmYeniEkle = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.ContextMenuStrip = this.contextMenuStrip2;
+            this.groupBox1.Controls.Add(this.btnGuncelle);
+            this.groupBox1.Controls.Add(this.btnKaydet);
+            this.groupBox1.Controls.Add(this.txtMail);
+            this.groupBox1.Controls.Add(this.txtTelefon);
+            this.groupBox1.Controls.Add(this.txtSoyadi);
+            this.groupBox1.Controls.Add(this.txtAdi);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(449, 237);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Personel Ekleme Paneli";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyadı : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Telefon : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Mail : ";
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(97, 45);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(326, 26);
+            this.txtAdi.TabIndex = 0;
+            // 
+            // txtSoyadi
+            // 
+            this.txtSoyadi.Location = new System.Drawing.Point(97, 77);
+            this.txtSoyadi.Name = "txtSoyadi";
+            this.txtSoyadi.Size = new System.Drawing.Size(326, 26);
+            this.txtSoyadi.TabIndex = 1;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(97, 109);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(326, 26);
+            this.txtTelefon.TabIndex = 2;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(97, 141);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(326, 26);
+            this.txtMail.TabIndex = 3;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(97, 173);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(160, 40);
+            this.btnKaydet.TabIndex = 4;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(263, 173);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(160, 40);
+            this.btnGuncelle.TabIndex = 5;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstKisiler);
+            this.groupBox2.Location = new System.Drawing.Point(13, 259);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(956, 303);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Personel Listesi";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtArama);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(469, 14);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(500, 237);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kontrol Paneli";
+            // 
+            // lstKisiler
+            // 
+            this.lstKisiler.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lstKisiler.ContextMenuStrip = this.contextMenuStrip1;
+            this.lstKisiler.FullRowSelect = true;
+            this.lstKisiler.GridLines = true;
+            this.lstKisiler.HideSelection = false;
+            this.lstKisiler.Location = new System.Drawing.Point(6, 25);
+            this.lstKisiler.Name = "lstKisiler";
+            this.lstKisiler.Size = new System.Drawing.Size(944, 272);
+            this.lstKisiler.TabIndex = 0;
+            this.lstKisiler.UseCompatibleStateImageBehavior = false;
+            this.lstKisiler.View = System.Windows.Forms.View.Details;
+            this.lstKisiler.DoubleClick += new System.EventHandler(this.tsmDuzenle_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Arama : ";
+            // 
+            // txtArama
+            // 
+            this.txtArama.Location = new System.Drawing.Point(80, 86);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(414, 26);
+            this.txtArama.TabIndex = 0;
+            this.txtArama.TextChanged += new System.EventHandler(this.txtArama_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmDuzenle,
+            this.tsmSil});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 48);
+            // 
+            // tsmDuzenle
+            // 
+            this.tsmDuzenle.Name = "tsmDuzenle";
+            this.tsmDuzenle.Size = new System.Drawing.Size(180, 22);
+            this.tsmDuzenle.Text = "Düzenle";
+            this.tsmDuzenle.Click += new System.EventHandler(this.tsmDuzenle_Click);
+            // 
+            // tsmSil
+            // 
+            this.tsmSil.Name = "tsmSil";
+            this.tsmSil.Size = new System.Drawing.Size(180, 22);
+            this.tsmSil.Text = "Sil";
+            this.tsmSil.Click += new System.EventHandler(this.tsmSil_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Adı";
+            this.columnHeader1.Width = 140;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Soyadı";
+            this.columnHeader2.Width = 192;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Telefon";
+            this.columnHeader3.Width = 144;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Mail";
+            this.columnHeader4.Width = 215;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmYeniEkle});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(150, 26);
+            // 
+            // tsmYeniEkle
+            // 
+            this.tsmYeniEkle.Name = "tsmYeniEkle";
+            this.tsmYeniEkle.Size = new System.Drawing.Size(180, 22);
+            this.tsmYeniEkle.Text = "Yeni Kayıt Ekle";
+            this.tsmYeniEkle.Click += new System.EventHandler(this.tsmYeniEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 574);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtSoyadi;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListView lstKisiler;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmDuzenle;
+        private System.Windows.Forms.ToolStripMenuItem tsmSil;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem tsmYeniEkle;
+    }
+}
+
---
+++ PhoneAppp/Form1.cs
@@ -0,0 +1,177 @@
+﻿using PhoneAppp.Models;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PhoneAppp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PhoneBookEntities db = new PhoneBookEntities();
+
+
+        void Clear()
+        {
+            foreach (Control item in groupBox1.Controls)
+            {
+                if (item is TextBox)
+                    item.Text = "";
+            }
+        }
+
+        void KisiListesi()
+        {
+            lstKisiler.Items.Clear();
+            var kisiler = db.People.ToList();
+
+            foreach (Person person in kisiler)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = person.FirstName;
+                lvi.SubItems.Add(person.LastName);
+                lvi.SubItems.Add(person.Phone);
+                lvi.SubItems.Add(person.Mail);
+                lvi.Tag = person.Id;
+
+                lstKisiler.Items.Add(lvi);
+            }
+        }
+
+        void KisiListesi(string param)
+        {
+            lstKisiler.Items.Clear();
+            var kisiler = db.People.Where(x =>
+
+             x.FirstName.Contains(param) ||
+             x.LastName.Contains(param) ||
+             x.Phone.Contains(param) ||
+             x.Mail.Contains(param)
+
+            ).ToList();
+
+            foreach (Person person in kisiler)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = person.FirstName;
+                lvi.SubItems.Add(person.LastName);
+                lvi.SubItems.Add(person.Phone);
+                lvi.SubItems.Add(person.Mail);
+                lvi.Tag = person.Id;
+
+                lstKisiler.Items.Add(lvi);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KisiListesi();
+        }
+
+        private void tsmYeniEkle_Click(object sender, EventArgs e)
+        {
+            txtAdi.Text = FakeData.NameData.GetFirstName();
+            txtSoyadi.Text = FakeData.NameData.GetSurname();
+            txtTelefon.Text = FakeData.PhoneNumberData.GetPhoneNumber();
+            txtMail.Text = $"{txtAdi.Text}.{txtSoyadi.Text}@{FakeData.NameData.GetCompanyName()}.com".ToLower().Replace(" ", "");
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Person person = new Person();
+            person.FirstName = txtAdi.Text;
+            person.LastName = txtSoyadi.Text;
+            person.Mail = txtMail.Text;
+            person.Phone = txtTelefon.Text;
+
+            db.People.Add(person);
+            bool result = db.SaveChanges() > 0;
+
+
+            //int count = db.SaveChanges();
+            //if (count > 0)
+            //{
+            //    result = true;
+            //}
+            //else
+            //{
+            //    result = false;
+            //}
+
+            result = 10 > 0;
+            KisiListesi();
+            Clear();
+
+            MessageBox.Show(result ? "Kayıt Eklendi" : "İşlem Hatası");
+        }
+
+        private void tsmSil_Click(object sender, EventArgs e)
+        {
+            if (lstKisiler.SelectedItems.Count > 0)
+            {
+
+                DialogResult dr = MessageBox.Show("Kişiyi silmek istiyormusunuz?\nİşlem geri alınamaz!", "Kişi Silme Bildirimi", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (dr == DialogResult.Yes)
+                {
+                    int id = (int)lstKisiler.SelectedItems[0].Tag;
+                    Person silinecek = db.People.Find(id);
+                    db.People.Remove(silinecek);
+                    db.SaveChanges();
+                    KisiListesi();
+                }
+                else
+                {
+                    MessageBox.Show("Kayıt Silme İşlemi İptal Edildi!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir kişi seçiniz!");
+            }
+        }
+
+
+
+        Person guncellenecek;
+        private void tsmDuzenle_Click(object sender, EventArgs e)
+        {
+            if (lstKisiler.SelectedItems.Count > 0)
+            {
+                int id = (int)lstKisiler.SelectedItems[0].Tag;
+                guncellenecek = db.People.Find(id);  // Find metodu primary key değerine göre size o kişi teslim eder.
+                txtAdi.Text = guncellenecek.FirstName;
+                txtSoyadi.Text = guncellenecek.LastName;
+                txtTelefon.Text = guncellenecek.Phone;
+                txtMail.Text = guncellenecek.Mail;
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+
+            if (guncellenecek == null)
+            {
+                MessageBox.Show("Lütfen bir kayıt seçiniz!");
+                return;
+            }
+            guncellenecek.FirstName = txtAdi.Text;
+            guncellenecek.LastName = txtSoyadi.Text;
+            guncellenecek.Mail = txtMail.Text;
+            guncellenecek.Phone = txtTelefon.Text;
+
+            db.SaveChanges();
+            Clear();
+            KisiListesi();
+            guncellenecek = null;
+        }
+
+        private void txtArama_TextChanged(object sender, EventArgs e)
+        {
+            KisiListesi(txtArama.Text);
+        }
+    }
+}
+
